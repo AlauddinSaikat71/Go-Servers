9c6fc9f41fb055694a6726345a41544c386fa248
AlauddinSaikat71 <alauddin27@student.sust.edu>
2021-04-21 21:54:58 +0600

    start w web server with GET routes

--- server.go
+++ server.go
@@ -2,9 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Printf("Starting server at port 8080\n")
 
+	//HandleFunc function to add route handlers to the web server
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello! Saikat")
+	})
+
+	fmt.Printf("Starting server at port 8080\n")
+	//This method allows us to start the web server and specify the port to listen for incoming requests.
+	//
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
