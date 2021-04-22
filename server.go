f54cb36dd2dd10a8458bb9def47ccaf045315e44
AlauddinSaikat71 <alauddin27@student.sust.edu>
2021-04-22 16:37:01 +0600

    add basic security to route

--- server.go
+++ server.go
@@ -6,12 +6,22 @@ import (
 	"net/http"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprint(w, "Hello!")
+}
+
 func main() {
 
 	//HandleFunc function to add route handlers to the web server
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello! Saikat")
-	})
+	http.HandleFunc("/hello", helloHandler)
 
 	fmt.Printf("Starting server at port 8080\n")
 	//This method allows us to start the web server and specify the port to listen for incoming requests.
