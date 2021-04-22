3e4460c45bcbe33cf3960b85a9f426aa3a8198b1
AlauddinSaikat71 <alauddin27@student.sust.edu>
2021-04-22 20:41:06 +0600

    accept a form submission POST request

--- server.go
+++ server.go
@@ -6,6 +6,19 @@ import (
 	"net/http"
 )
 
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm err : %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
+}
+
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/hello" {
 		http.Error(w, "404 not found", http.StatusNotFound)
@@ -24,6 +37,7 @@ func main() {
 
 	//HandleFunc function to add route handlers to the web server
 	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/form", formHandler)
 
 	fmt.Printf("Starting server at port 8080\n")
 	//This method allows us to start the web server and specify the port to listen for incoming requests.
